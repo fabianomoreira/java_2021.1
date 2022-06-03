c3b09dd586778fedf5dea36956ab42bb17db1f29
fabianomoreira <professor@fabianomoreira.com.br>
2022-06-03 11:43:47 -0300

    Implementações da aula 11

---
+++ 11_aula/src/main/java/controle/LoginServlet.java
@@ -0,0 +1,38 @@
+package controle;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import servico.ServicoLogin;
+
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LoginServlet() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		String usuario = request.getParameter("usuario"); 
+		String senha =  request.getParameter("senha");
+		
+		ServicoLogin servico = new ServicoLogin();
+		
+		if(servico.verificarLogin(usuario, senha)) {
+			response.sendRedirect("menu.html");
+		} else {
+			response.sendRedirect("erro.html");
+		}
+	}
+
+}
