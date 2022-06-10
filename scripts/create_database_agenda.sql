2b50ebabbab4e6f6d3212eb64759195ccb62ef70
fabianomoreira <professor@fabianomoreira.com.br>
2022-06-10 11:46:57 -0300

    Implementações da aula 14

--- scripts/create_database_agenda.sql
+++ scripts/create_database_agenda.sql
@@ -10,5 +10,13 @@ CREATE TABLE tb_usuario(
 );
 
 INSERT INTO tb_usuario(nome, usuario, senha) VALUES('Administrador', 'admin', '12345');
+INSERT INTO tb_usuario(nome, usuario, senha) VALUES('Convidado', 'convidado', '12345');
+INSERT INTO tb_usuario(nome, usuario, senha) VALUES('Estagiário', 'estag', '12345');
 
 SELECT * FROM tb_usuario;
+
+ALTER TABLE tb_usuario ADD COLUMN foto VARCHAR(100);
+
+UPDATE tb_usuario SET foto="images/user1.png" WHERE id=1;
+UPDATE tb_usuario SET foto="images/user2.png" WHERE id=2;
+UPDATE tb_usuario SET foto="images/user2.png" WHERE id=3;
